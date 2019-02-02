44ec545c2f0ecc1977f92c3fa4e7d0a722a16511
Richiearnold <richard@ambercouch.co.uk>
2019-02-02 06:43:19 +0000

    update Script and styles

--- assets/js/ac-js-calendar.js
+++ assets/js/ac-js-calendar.js
@@ -42,7 +42,24 @@ function calTemplate(){
 
 </div>
 </div>
-<p>Some Calendar</p>
+<div class="c-ac-calendar__body">
+<div class="c-ac-calendar__date-list">
+<div class="c-date-list">
+<ul class="c-date-list__list--heading">
+<li class="c-date-list__item--heading">m</li>
+<li class="c-date-list__item--heading">t</li>
+<li class="c-date-list__item--heading">w</li>
+<li class="c-date-list__item--heading">t</li>
+<li class="c-date-list__item--heading">f</li>
+<li class="c-date-list__item--heading">s</li>
+<li class="c-date-list__item--heading">s</li>
+</ul>
+<ul id='monthDaysList' class="c-date-list__list--month-days" data-month="" >
+
+</ul>
+</div>
+</div>
+</div>
 `;
 }
 
@@ -51,11 +68,23 @@ let calContainer = document.querySelector("[data-ac-calendar]");
 
 let calElement = document.createElement('div');
 
+let calDayItemEl = document.createElement('li');
+
 let yearDownButton;
 
 let yearUpButton;
 
 let yearHeading;
+
+let monthDownButton;
+
+let monthUpButton;
+
+let monthHeading;
+
+let monthDaysList;
+
+
 
 let year = (new Date()).getFullYear();
 let month = (new Date()).getMonth();
@@ -68,6 +97,9 @@ eventYearUpdate.initEvent('yearUpdate', true, true);
 
 let eventMonthUpdate = document.createEvent('Event');
 eventMonthUpdate.initEvent('monthUpdate', true, true);
+
+let eventMonthDaysUpdate = document.createEvent('Event');
+eventMonthDaysUpdate.initEvent('monthDaysUpdate', true, true);
 
 // var eventActClose = document.createEvent('Event');
 // eventActClose.initEvent('actClose', true, true);
@@ -84,70 +116,168 @@ let calDat = {
     selectedYear : '',
     currentMonth : '',
     selectedMonth : '',
+    currentDay : '',
+    daysInMonth : '',
+    firstDayOfMonth: '',
+    init: function(){
+        this.setCurrentYear()
+        this.setSelectedYear()
+        
+        this.setCurrentMonth()
+        this.setSelectedMonth()
+
+        this.setCurrentDay()
+
+
+
+
+
+        this.setDaysInMonth();
+
+        this.setFirstDayOfmonth();
+
+        calElement.className = 'c-ac-calendar'
+        calElement.innerHTML= calTemplate()
+
+        calContainer.appendChild(calElement);
+
+        yearDownButton = document.getElementById("yearDownButton");
+        yearUpButton = document.getElementById("yearUpButton");
+        yearHeading = document.getElementById("yearHeading");
+
+        monthDaysList = document.getElementById('monthDaysList')
+
+        this.buildDaysGrid();
+        this.buildDays();
+
+        this.addTodayClass();
+
+
+
+    },
     getCurrentYear : function(){
         return (new Date()).getFullYear();
     },
     getSelectedYear : function(){
-        return calDat.selectedYear;
+        return this.selectedYear;
     },
     setCurrentYear : function() {
-        calDat.currentYear = (new Date()).getFullYear();
+        this.currentYear = (new Date()).getFullYear();
     },
-    setSelectedYear : function(year = calDat.getCurrentYear()) {
-        calDat.selectedYear = year;
+    setSelectedYear : function(year = this.getCurrentYear()) {
+        this.selectedYear = year;
+        console.log('selected year' + year);
+        window.dispatchEvent(eventYearUpdate);
     },
     getCurrentMonth : function(){
         return (new Date()).getMonth();
     },
     getSelectedMonth : function(){
-        return calDat.selectedMonth;
+        return this.selectedMonth;
     },
     setCurrentMonth : function() {
-        calDat.currentMonth = (new Date()).getMonth();
+        this.currentMonth = (new Date()).getMonth();
     },
-    setSelectedMonth : function(month = calDat.getCurrentMonth()) {
+    setSelectedMonth : function(month = this.getCurrentMonth()) {
         if (month < 0){
-            month = 10 - month;
-            return calDat.setSelectedMonth(month);
+            month =  month + 12;
+            return this.setSelectedMonth(month);
         }else if(month > 11){
             month = month - 12;
-            return calDat.setSelectedMonth(month);
+            return this.setSelectedMonth(month);
         }
-        calDat.selectedMonth = month;
+        if (this.selectedMonth == 0 && month == 11){
+            this.setSelectedYear( this.getSelectedYear() - 1)
+        }else if(this.selectedMonth == 11 && month == 0){
+
+            this.setSelectedYear( this.getSelectedYear() + 1)
+        }
+        this.selectedMonth = month;
+        window.dispatchEvent(eventMonthUpdate);
+    },
+    getCurrentDay: function(){
+        return (new Date()).getDate();
+    },
+    setCurrentDay: function (){
+      this.currentDay = this.getCurrentDay();
+    },
+    setDaysInMonth : function (month = this.selectedMonth, year = this.selectedYear) {
+        //year = selected year
+        //month + 1 = month after selected month
+        //day 0 = last day of previous month
+        let day = 0;
+        this.daysInMonth = new Date(year, month + 1, day).getDate();
+        window.dispatchEvent(eventMonthDaysUpdate);
+    },
+    setFirstDayOfmonth: function (month = this.selectedMonth, year = this.selectedYear) {
+        this.firstDayOfMonth = new Date(year, month, 1).getDay();
+    },
+    buildDaysGrid: function () {
+        calDayItemEl.className = 'c-date-list__item--month-days is-empty';
+        calDayItemEl.innerText = '';
+        for (let i = 0; i < 35; i++) {
+            calDayItemEl = calDayItemEl.cloneNode(true)
+            calDayItemEl.dataset.elIndex = i;
+
+            calDayItemEl.id = 'dayIndex' + i;
+
+            monthDaysList.appendChild(calDayItemEl);
+        }
+    },
+    clearDaysGrid: function () {
+        monthDaysList.innerHTML = '';
+    },
+    buildDays: function () {
+        let startDay = this.firstDayOfMonth - 1;
+        if (startDay < 0){
+            startDay = 6
+        }
+        console.log('startDay');
+        console.log(startDay);
+        for (let i = 0; i < this.daysInMonth; i++) {
+            let day = i + 1;
+
+            let offset = startDay  + i;
+            let el = document.getElementById('dayIndex' + offset)
+            el.innerText = day;
+            el.dataset.dayOfMonth = day;
+            el.classList.remove('is-empty');
+        }
+    },
+    addTodayClass: function (){
+        if (this.selectedYear == this.currentYear && this.selectedMonth == this.currentMonth){
+            el = document.querySelectorAll('[data-day-of-month="'+ calDat.currentDay +'"]');
+            el[0].classList.add("is-today");
+            console.log(el);
+        }
+    },
+    updateCalendar: function () {
+        this.setDaysInMonth();
+        this.setFirstDayOfmonth();
+        this.clearDaysGrid();
+        this.buildDaysGrid();
+        this.buildDays();
     }
+
 }
-calDat.setCurrentYear()
-calDat.setSelectedYear()
 
-calDat.setCurrentMonth()
-calDat.setSelectedMonth()
 
+
+calDat.init();
 console.log(calDat.selectedYear)
-calDat.setCurrentYear();
 
-
-
-
-calElement.className = 'c-ac-calendar'
-calElement.innerHTML= calTemplate()
-
-calContainer.appendChild(calElement);
-
-yearDownButton = document.getElementById("yearDownButton");
-yearUpButton = document.getElementById("yearUpButton");
-yearHeading = document.getElementById("yearHeading");
 
 yearDownButton.addEventListener('click', function(){
     calDat.setSelectedYear( calDat.getSelectedYear() - 1)
-    window.dispatchEvent(eventYearUpdate);
+    console.log(calDat.getSelectedYear())
 });
 yearUpButton.addEventListener('click', function(){
     calDat.setSelectedYear( calDat.getSelectedYear() + 1)
-    window.dispatchEvent(eventYearUpdate);
 });
 
 window.addEventListener('yearUpdate', function (e) {
     yearHeading.innerText = calDat.selectedYear;
+    calDat.updateCalendar();
 })
 
 monthDownButton = document.getElementById("monthDownButton");
@@ -156,14 +286,12 @@ monthHeading = document.getElementById("monthHeading");
 
 monthDownButton.addEventListener('click', function(){
     calDat.setSelectedMonth( calDat.getSelectedMonth() - 1)
-    window.dispatchEvent(eventMonthUpdate);
 });
 monthUpButton.addEventListener('click', function(){
     calDat.setSelectedMonth( calDat.getSelectedMonth() + 1)
-    window.dispatchEvent(eventMonthUpdate);
 });
 
 window.addEventListener('monthUpdate', function (e) {
     monthHeading.innerText = calDat.monthNames[calDat.selectedMonth];
-    console.log(calDat.selectedMonth)
+    calDat.updateCalendar();
 })
